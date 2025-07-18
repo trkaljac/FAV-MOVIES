1c5a181d1ac26ae2b4280cbf056b6e400d6e92ed
trkaljac <103012052+trkaljac@users.noreply.github.com>
2025-07-17 20:48:57 -0500

    dinamicly render using .map

--- Fav-Movies/src/App.jsx
+++ Fav-Movies/src/App.jsx
@@ -7,16 +7,7 @@ function App() {
 
   return (
     <>
-      <MovieCard movie={{
-        title: "Inception",
-        year: "2010",
-        poster: "https://example.com/inception.jpg"
-      }} />
-            <MovieCard movie={{
-        title: "Interstellar",
-        year: "2014",
-        poster: "https://example.com/interstellar.jpg"
-      }} />
+      <Home></Home>
     </>
   )
 }

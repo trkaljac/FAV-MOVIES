13ccd7ffb62fa295e454da0f46884cb8023fd440
trkaljac <103012052+trkaljac@users.noreply.github.com>
2025-07-18 00:05:29 -0500

    added on favorite click so it can send the data to favorite page so it can display favorite movies

--- Fav-Movies/src/components/MovieCard.jsx
+++ Fav-Movies/src/components/MovieCard.jsx
@@ -1,21 +1,29 @@
 import '../css/MovieCard.css';
+import { useMovieContext } from '../contexts/MovieContext';
 function MovieCard({ movie }) {
-  function handleFavoriteClick() {
-    alert("clicked");
+  const {isFavorite,addToFavorites, removeFromFavorites} = useMovieContext();
+  const favorite = isFavorite(movie.id);
+
+    function onFavoriteClick(e) {
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
 
   }
-    // Logic to handle favorite click
+    
 
   return <div className="movie-card">
       <div className="movie-poster">
          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
         <div className="movie-overlay">
-          <button className="favorite-btn" onClick={handleFavoriteClick}>♡</button>
+          <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                    ♥
+          </button>
         </div>
       </div>
       <div className="movie-info ">
         <h3 className="movie-title">{movie.title}</h3>
-        <p className="movie-year">{movie.year}</p>
+        <p className="movie-year">{movie.release_date}</p>
       </div>
     </div>
 }

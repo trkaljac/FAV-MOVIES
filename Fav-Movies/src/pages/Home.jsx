ceb425634ec66a31c58c4c5059a6be747e1a8708
trkaljac <103012052+trkaljac@users.noreply.github.com>
2025-07-17 20:46:25 -0500

    added movie list

---
+++ Fav-Movies/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import MovieCard from "../components/MovieCard";
+function Home() {
+  const movies = [
+    {id: 1, title: "Inception", year: "2010", poster: "https://example.com/inception.jpg" },
+    {id: 2, title: "Interstellar", year: "2014", poster: "https://example.com/interstellar.jpg" },
+    {id: 3, title: "The Dark Knight", year: "2008", poster: "https://example.com/darkknight.jpg"  },
+    {id: 4, title: "The Matrix", year: "1999", poster: "https://example.com/matrix.jpg"  }
+  ];  
+  return <div className="home">
+      <div className="movie-grid">
+        {movies.map(movie => (<MovieCard key={movie.id} movie={movie} />))}
+      </div>
+    </div>
+  
+}
+
+export default Home;

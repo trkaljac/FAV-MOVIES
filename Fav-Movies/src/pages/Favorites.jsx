43ffedd78413b2bbb4a09df0c55fa300a7112dd4
trkaljac <103012052+trkaljac@users.noreply.github.com>
2025-07-17 21:54:14 -0500

    added <h1> for testing

---
+++ Fav-Movies/src/pages/Favorites.jsx
@@ -0,0 +1,11 @@
+function Favorites() {
+  return (
+    <div>
+      <h1>Favorites Page</h1>
+      <p>This is where your favorite movies will be displayed.</p>
+      {/* Add your favorite movies component here */}
+    </div>
+  );
+}
+
+export default Favorites;

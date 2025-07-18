2512aba841cd1605b447da69f3cf0532c892a2c3
trkaljac <103012052+trkaljac@users.noreply.github.com>
2025-07-18 00:11:03 -0500

    imported movie provider

--- Fav-Movies/src/main.jsx
+++ Fav-Movies/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { MovieProvider } from "./contexts/MovieContext";
 import './css/index.css'
 import App from './App.jsx'
 

41bc3a44890d89ee159d5d639d1b9d5556653791
trkaljac <103012052+trkaljac@users.noreply.github.com>
2025-07-17 19:54:46 -0500

    added MovieCards to display on the page

--- Fav-Movies/src/App.jsx
+++ Fav-Movies/src/App.jsx
@@ -6,10 +6,16 @@ function App() {
 
   return (
     <>
-      <div>
-        
-      </div>
-      
+      <MovieCard movie={{
+        title: "Inception",
+        year: "2010",
+        poster: "https://example.com/inception.jpg"
+      }} />
+            <MovieCard movie={{
+        title: "Interstellar",
+        year: "2014",
+        poster: "https://example.com/interstellar.jpg"
+      }} />
     </>
   )
 }

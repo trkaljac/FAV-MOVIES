a4364b573d06172cb9aa3a496c314ec8e48db421
trkaljac <103012052+trkaljac@users.noreply.github.com>
2025-07-18 00:06:35 -0500

    edited the code so favorite movies can be displayed

--- Fav-Movies/src/pages/Favorites.jsx
+++ Fav-Movies/src/pages/Favorites.jsx
@@ -1,10 +1,27 @@
 import'../css/Favorites.css';
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
 function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (Array.isArray(favorites) && favorites.length > 0) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Favorites Page</h1>
-      <p>This is where your favorite movies will be displayed.</p>
-      {/* Add your favorite movies component here */}
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here!</p>
     </div>
   );
 }

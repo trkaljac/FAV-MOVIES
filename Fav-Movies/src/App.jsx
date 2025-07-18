bf3211bd2243233d399baf2d21f031a3f549fdf7
trkaljac <103012052+trkaljac@users.noreply.github.com>
2025-07-17 21:40:25 -0500

    added a path to home and favorites

--- Fav-Movies/src/App.jsx
+++ Fav-Movies/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
+import Favorites from './pages/Favorites'
 import Home from './pages/Home'
+import { Route,Routes } from 'react-router-dom'
 
 function App() {
   
 
   return (
-    <>
-      <Home></Home>
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites/" element={<Favorites />} />
+      </Routes>
+    </main>
   )
 }
 
